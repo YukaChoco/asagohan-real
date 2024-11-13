aca9476a833a5e8b4c83db848a71119dac74db06
YukaChoco <sugar.ore.yk@gmail.com>
2024-11-14 00:58:38 +0900

    コメントアウト戻し忘れた

--- app/utils/sendComment.ts
+++ app/utils/sendComment.ts
@@ -6,28 +6,28 @@ const sendComment = async (
   console.log(userID, asagohanID, comment);
 
   // コメントを送信するためのAPIを叩く(以下、fetchの使い方)
-  await fetch("/api/asagohan/comment", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      created_at: new Date(),
-      userID,
-      asagohanID,
-      comment,
-    }),
-  })
-    // 成功した場合はコンソールに成功メッセージを表示
-    .then((res) => {
-      if (!res.ok) {
-        console.error("Failed to send comment");
-      }
-    })
-    // 失敗した場合はエラーメッセージを表示
-    .catch((error) => {
-      console.error(error);
-    });
+  // await fetch("/api/asagohan/comment", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     created_at: new Date(),
+  //     userID,
+  //     asagohanID,
+  //     comment,
+  //   }),
+  // })
+  //   // 成功した場合はコンソールに成功メッセージを表示
+  //   .then((res) => {
+  //     if (!res.ok) {
+  //       console.error("Failed to send comment");
+  //     }
+  //   })
+  //   // 失敗した場合はエラーメッセージを表示
+  //   .catch((error) => {
+  //     console.error(error);
+  //   });
 
   return;
 };
