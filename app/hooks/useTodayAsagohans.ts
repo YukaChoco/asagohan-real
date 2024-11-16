b4e52421d18fcfca816eaa0437359806591304ad
YukaChoco <sugar.ore.yk@gmail.com>
2024-11-17 04:04:20 +0900

    消し逃しbody削除

--- app/hooks/useTodayAsagohans.ts
+++ app/hooks/useTodayAsagohans.ts
@@ -8,13 +8,7 @@ const useTodayAsagohans = (userID: string | null, authLoading: boolean) => {
 
   const getTodayAsagohans = async (userID: string): Promise<Asagohan[]> => {
     console.log("fetching asagohans...");
-    const res = await fetch(`/api/asagohans/${userID}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ dateString: new Date().toISOString() }),
-    });
+    const res = await fetch(`/api/asagohans/${userID}`);
     console.log("res:", res);
 
     if (!res.ok) {
