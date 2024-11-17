3111d1ff93b6f86a7c5062aa57240cfc514f8cdf
YukaChoco <sugar.ore.yk@gmail.com>
2024-11-17 11:59:40 +0900

    再度token取得成功

--- app/firebase.ts
+++ app/firebase.ts
@@ -16,7 +16,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 console.log("Firebase app initialized:", app);
 
 export const auth = getAuth(app);
---
+++ app/hooks/useFCM.ts
@@ -0,0 +1,45 @@
+"use client";
+import { useEffect, useState } from "react";
+import { getMessaging, getToken } from "firebase/messaging";
+import { app } from "../firebase";
+
+const vapidKey = process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY;
+
+const useFCM = () => {
+  const [fcmToken, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const messaging = getMessaging(app);
+    getToken(messaging, {
+      vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
+    });
+
+    // tokenを取得
+    const getUserToken = async () => {
+      try {
+        const currentToken = await getToken(messaging, { vapidKey });
+        if (currentToken) {
+          console.log("FCM Token:", currentToken);
+          setToken(currentToken);
+          // サーバーにトークンを送信
+          //   await fetch("/api/save-token", {
+          //     method: "POST",
+          //     headers: {
+          //       "Content-Type": "application/json",
+          //     },
+          //     body: JSON.stringify({ token: currentToken }),
+          //   });
+        } else {
+          console.log("No registration token available.");
+        }
+      } catch (err) {
+        console.error("An error occurred while retrieving token.", err);
+      }
+    };
+    getUserToken();
+  }, []);
+
+  return {};
+};
+
+export default useFCM;
